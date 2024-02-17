0104694b828a77e8f3bef4f43c6c80eadd5918ca
Nafeu Nasir <nafeu.nasir@gmail.com>
2024-02-16 22:51:30 -0500

    Update data.js with bash snippets

--- data.js
+++ data.js
@@ -22,4 +22,29 @@ const debounce = (func, delay) => {
 
 element.addEventListener('event', debounce(() => {}, 500))
 ---
+bash remove everything in directory | bash remove rm all wildcard
+rm /path/to/dir/*
+---
+bash script capture or handle arguments | bash script capture handle arguments command-line terminal
+#!/bin/bash
+
+# Print the script name and args 1 & 2
+echo "Script name: $0"
+echo "Arg 1: $1"
+echo "Arg 2: $2"
+
+# Print the number of arguments
+echo "Number of arguments: $#"
+
+# Print all arguments
+echo "All arguments: $@"
+
+# Loop through all arguments and print them
+for arg in "$@"
+do
+  echo "Argument: $arg"
+done
+
+# > chmod +x script.sh
+# > ./script.sh arg1 arg2 arg3
 `
